d687b86209246e7131cd6da38b412b2b2cf7885c
wwx <>
2025-12-18 16:22:43 +0800

    学号

--- sustc-api/src/main/java/io/sustc/service/impl/DatabaseServiceImpl.java
+++ sustc-api/src/main/java/io/sustc/service/impl/DatabaseServiceImpl.java
@@ -44,7 +44,7 @@ public class DatabaseServiceImpl implements DatabaseService {
     @Override
     public List<Integer> getGroupMembers() {
         //TODO: replace this with your own student IDs in your group
-        return Arrays.asList(12210000, 12210001, 12210002);
+        return Arrays.asList(12411011, 12411024);
     }
 
     @Autowired
