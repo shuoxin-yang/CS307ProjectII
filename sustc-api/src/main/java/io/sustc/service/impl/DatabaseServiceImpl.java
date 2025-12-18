3467d27a2c979fe6742f95284dd81313bccd1662
Yuhui Yang <3325444898@qq.com>
2025-12-18 16:29:19 +0800

    1

--- sustc-api/src/main/java/io/sustc/service/impl/DatabaseServiceImpl.java
+++ sustc-api/src/main/java/io/sustc/service/impl/DatabaseServiceImpl.java
@@ -44,7 +44,7 @@ public class DatabaseServiceImpl implements DatabaseService {
     @Override
     public List<Integer> getGroupMembers() {
         //TODO: replace this with your own student IDs in your group
-        return Arrays.asList(12411011, 12411024);
+        return Arrays.asList(0000000, 12411024);
 
     }
 
